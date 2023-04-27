ee60190994fef39581df7f5b065c3f96ab9779d2
simone-meneses <monymenese.santos@hotmail.com>
2023-04-26 21:12:58 -0300

    feat: :ambulance: installing parse library to organize wordpress posts

--- back-end/dev/wp-config.php
+++ back-end/dev/wp-config.php
@@ -63,6 +63,7 @@ define( 'DB_CHARSET', getenv_docker('WORDPRESS_DB_CHARSET', 'utf8') );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', getenv_docker('WORDPRESS_DB_COLLATE', '') );
+define('FS_METHOD','direct');
 
 /**#@+
  * Authentication unique keys and salts.
--- front-end/src/posts/index.js
+++ front-end/src/posts/index.js
@@ -1,29 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import parse from 'html-react-parser';
 
 const Posts = () => {
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
 
   useEffect(() => {
-    axios.get('http://localhost:8000/wp-json/wp/v2/posts')
-      .then(response => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/wp-json/wp/v2/posts');
         const data = response.data.map(post => ({
           ...post,
-          content: post.content.rendered.replace(/<!--[\S\S]*?-->/g, '').replace(/<\/?p>/g, '').replace(/<\/?li>/g, '').replace(/<\/?ol>/g, '')
+          content: parse(post.content.rendered)
         }));
+        
         setPosts(data);
-      });
+        setIsLoading(false);
+      } catch (error) {
+        setHasError(true);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
   }, []);
 
+  const renderPosts = () => {
+    if (isLoading) {
+      return <p>Carregando...</p>;
+    }
+
+    if (hasError) {
+      return <p>Ocorreu um erro ao carregar os posts.</p>;
+    }
+
+    return (
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>
+            <h2>{post.title.rendered}</h2>
+            <p>{post.content}</p>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
-    <ul>
-      {posts.map(post => (
-        <li key={post.id}>
-          <h2>{post.title.rendered}</h2>
-          <p>{post.content}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      {renderPosts()}
+    </>
   );
 };
 
